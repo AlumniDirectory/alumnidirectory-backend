9f3ff7c0f69d411108cac6ed8c81fb407aa791a3
Michael Yosavani Putra <surbaktievan@ymail.com>
2018-09-17 14:49:19 +0700

    benerin yg rusak

--- api/models/batches.js
+++ api/models/batches.js
@@ -1,6 +1,6 @@
 /* jshint indent: 2 */
 
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   let batches = sequelize.define(
     "batches",
     {
@@ -15,6 +15,9 @@ module.exports = function (sequelize, DataTypes) {
       endDate: {
         type: DataTypes.DATEONLY,
         allowNull: false
+      },
+      image: {
+        type: DataTypes.STRING(20)
       }
     },
     {
@@ -23,12 +26,12 @@ module.exports = function (sequelize, DataTypes) {
     }
   );
 
-  batches.associate = function (models) {
+  batches.associate = function(models) {
     models.batches.hasMany(models.students, {
-      foreignKey: 'id_batch',
+      foreignKey: "id_batch",
       as: "students"
     });
   };
 
-  return batches
+  return batches;
 };
